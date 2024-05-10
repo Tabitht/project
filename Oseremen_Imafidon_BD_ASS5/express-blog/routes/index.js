2b37e41aea746cf2ad99710ad25f076145e463a5
Tabitht <imafidontabitha@gmail.com>
2024-05-10 21:06:46 +0100

    Made little correction

--- Oseremen_Imafidon_BD_ASS5/express-blog/routes/index.js
+++ Oseremen_Imafidon_BD_ASS5/express-blog/routes/index.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let navigation_link =
-   ['Basic Industries', 'Consumer Services', 'Technology', 'Health Care', 'BioTech' ];
-
+  
   let rawdata = fs.readFileSync('./database/posts.json');
 
   let posts = JSON.parse(rawdata);
 
-  let featuredPosts = posts.filter('is_featured')
+  let navigation_link = [...new Set(posts.map(post => post.category))]
+
+  let featuredPosts = posts.filter(post => post.is_featured)
 
   res.render('blog', {
     title: 'She Code Queens',
