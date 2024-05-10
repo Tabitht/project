c69d7812646d7d16b3ce4733e33d81f594fbda18
Tabitht <imafidontabitha@gmail.com>
2024-05-10 20:45:35 +0100

    Extracted the categories

--- Oseremen_Imafidon_BD_ASS5/express-blog/routes/index.js
+++ Oseremen_Imafidon_BD_ASS5/express-blog/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('blog', { title: 'GTCO food and drinks' });
+  let navigation_link =
+   ['Basic Industries', 'Consumer Services', 'Technology', 'Health Care', 'BioTech' ];
+
+  let rawdata = fs.readFileSync('./database/posts.json');
+
+  let posts = JSON.parse(rawdata);
+
+  let featuredPosts = posts.filter('is_featured')
+
+  res.render('blog', {
+    title: 'She Code Queens',
+    links: navigation_link,
+    posts: posts
+});
+
 });
 
 module.exports = router;
