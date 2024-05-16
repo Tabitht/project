e07d624502b07fb796cc2da2633b44c0842443ef
Tabitht <imafidontabitha@gmail.com>
2024-05-16 16:42:36 -0500

    Formatted the date

--- Oseremen_Imafidon_BD_ASS5/express-blog/routes/index.js
+++ Oseremen_Imafidon_BD_ASS5/express-blog/routes/index.js
@@ -1,23 +1,43 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
+const { parse, isSameDay, isBefore } = require("date-fns");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
   let rawdata = fs.readFileSync('./database/posts.json');
 
-  let data = JSON.parse(rawdata);
+  let data = JSON.parse(rawdata).sort( function (a, b) {
+    let dateOne = parse(a.created_at, 'yyyy-MM-dd', new Date())
+
+    let dateTwo = parse(b.created_at, 'yyyy-MM-dd', new Date());
+
+    if (isSameDay(dateOne, dateTwo)) {
+      return 0;
+    }
+
+    if (isBefore(dateOne, dateTwo)) {
+      return -1;
+    }
+
+    return -1;
+  });
 
   let navigation_link = [...new Set(data.map(post => post.category).sort())]
+
+  let dates = date.map(function(post) {
+    let [year, month] = post.created_at.split('-');
+    return new Date( `${year}-${month}-01`);
+  });
 
 
   res.render('blog', {
     title: 'She Code Queens',
     links: navigation_link,
     posts: data.filter(post => ! post.is_featured),
-    featuredPosts: data.filter(post => post.is_featured)
-
+    featuredPosts: data.filter(post => post.is_featured),
+    archives: Array.from(new Set(dates))
 });
 
 });
